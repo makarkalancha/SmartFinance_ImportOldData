332921c0a333b7ef86687c01dc98030773189c3c
makarkalancha@gmail.com <makarkalancha@gmail.com>
2016-03-21 15:23:50 -0400

    enhanced-sequence: doesn't work with h2 db

---
+++ database_layer/src/main/java/com/makco/smartfinance/db/Constants.java
@@ -0,0 +1,9 @@
+package com.makco.smartfinance.db;
+
+/**
+ * Created by mcalancea on 2016-03-21.
+ */
+public interface Constants {
+
+    public static final String ID_GENERATOR = "ID_GENERATOR";
+}
