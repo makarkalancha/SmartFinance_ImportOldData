e1e37f9b61c15a9d607875db5ea956df7bebe9c3
makarkalancha@gmail.com <makarkalancha@gmail.com>
2016-03-21 16:43:36 -0400

    revert enhanced-sequence with old/new mapping true/false

--- database_layer/src/main/java/com/makco/smartfinance/db/Constants.java
+++
@@ -1,9 +0,0 @@
-package com.makco.smartfinance.db;
-
-/**
- * Created by mcalancea on 2016-03-21.
- */
-public interface Constants {
-
-    public static final String ID_GENERATOR = "ID_GENERATOR";
-}
