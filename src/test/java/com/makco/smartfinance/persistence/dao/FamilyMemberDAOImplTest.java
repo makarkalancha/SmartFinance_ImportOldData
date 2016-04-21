d8f81a940e813c44c6fcafbef433b92f77af6a5d
Makar Kalancha <makarkalancha@gmail.com>
2016-04-20 21:57:21 -0400

    dao test: todo compare different strategies like merge-detach vs saveOrUpdate

---
+++ src/test/java/com/makco/smartfinance/persistence/dao/FamilyMemberDAOImplTest.java
@@ -0,0 +1,16 @@
+package com.makco.smartfinance.persistence.dao;
+
+import static org.junit.Assert.*;
+
+/**
+ * User: Makar Kalancha
+ * Date: 20/04/2016
+ * Time: 21:40
+ */
+public class FamilyMemberDAOImplTest {
+    // TODO
+    // hibernate persistence book p281
+    // session.saveOrUpdate no, better use detach-merge approach
+    // compare if this approach uses less queries
+
+}
