a3b81f77a2f6786682626c66bd8c559f177ad071
Makar Kalancha <makarkalancha@gmail.com>
2016-05-24 22:39:56 -0400

    Transaction dao todo

---
+++ src/test/java/com/makco/smartfinance/persistence/dao/TransactionDAOImplTest.java
@@ -0,0 +1,10 @@
+package com.makco.smartfinance.persistence.dao;
+
+/**
+ * User: Makar Kalancha
+ * Date: 24/05/2016
+ * Time: 22:23
+ */
+public class TransactionDAOImplTest {
+    //TODO p386 and section 20.1 cursor and batch processing
+}
