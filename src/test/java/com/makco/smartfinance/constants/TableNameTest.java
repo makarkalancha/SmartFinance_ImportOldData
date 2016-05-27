26fb49a053282f795d27980eb76b45f6ebfea8dc
makarkalancha@gmail.com <makarkalancha@gmail.com>
2016-05-27 16:21:48 -0400

    table name annotations

---
+++ src/test/java/com/makco/smartfinance/constants/TableNameTest.java
@@ -0,0 +1,16 @@
+package com.makco.smartfinance.constants;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by mcalancea on 2016-05-27.
+ */
+public class TableNameTest {
+
+/**
+ * todo somehow entity table hibernate table annotation should take
+ * the table name from the source as data_base project
+ */
+
+
+}
