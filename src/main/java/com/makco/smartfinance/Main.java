d9b3d835cd3cd53ff51ec482c1d1eec2248a8185
Makar Kalancha <makarkalancha@gmail.com>
2016-06-18 23:18:25 -0400

    main

--- src/main/java/com/makco/smartfinance/Main.java
+++ src/main/java/com/makco/smartfinance/Main.java
@@ -151,7 +151,7 @@ public class Main extends Application{
             for (Screens scr : Screens.values()) {
                 mainContainer.loadScreen(scr);
             }
-            mainContainer.setScreen(Screens.ORGANIZATION);
+            mainContainer.setScreen(Screens.CURRENCY);
 
             this.primaryStage.getIcons().add(new Image(UserInterfaceConstants.MAIN_WINDOW_ICO));
             this.primaryStage.setTitle(UserInterfaceConstants.MAIN_WINDOW_TITLE);
