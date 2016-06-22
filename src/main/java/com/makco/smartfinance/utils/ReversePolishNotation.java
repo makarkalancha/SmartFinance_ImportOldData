ba61b8191adff204d0adf4f7cd2f3e09b95c9f46
Makar Kalancha <makarkalancha@gmail.com>
2016-06-21 23:02:07 -0400

    reverse polish notation

--- src/main/java/com/makco/smartfinance/utils/ReversePolishNotation.java
+++ src/main/java/com/makco/smartfinance/utils/ReversePolishNotation.java
@@ -11,7 +11,41 @@ public class ReversePolishNotation {
 
     public static String convertToReversePolishNotation(String arithmeticNotation){
         StringBuilder reversePolishNotation = new StringBuilder();
-        char[] chars = ari
+        Stack<Character> characterStack = new DequeStack<>();
+//        arithmeticNotation.chars().forEach(intConsumer -> {
+        for(char intConsumer : arithmeticNotation.toCharArray()) {
+            System.out.println(">>intConsumer:" + intConsumer);
+            switch ((char) intConsumer) {
+                case '+':
+                case '-':
+                case '*':
+                case '/':
+                case '(':
+                    characterStack.push(Character.valueOf((char) intConsumer));
+                    break;
+//                case '*':
+//                case '/':
+//                    reversePolishNotation.append(intConsumer);
+//                    break;
+                case ')':
+                    reversePolishNotation.append(characterStack.pop().toString());
+                    break;
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                case '0':
+                    reversePolishNotation.append(intConsumer);
+                    break;
+            }
+//        });
+        }
+        System.out.println(">>>characterStack: " + characterStack);
         return reversePolishNotation.toString();
     }
 
---
+++ src/test/java/com/makco/smartfinance/utils/ReversePolishNotationTest.java
@@ -0,0 +1,30 @@
+package com.makco.smartfinance.utils;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * User: Makar Kalancha
+ * Date: 21/06/2016
+ * Time: 22:41
+ */
+public class ReversePolishNotationTest {
+
+    @Test
+    public void test_convert_1() throws Exception{
+        String formula = "1+2";
+        String factResult = ReversePolishNotation.convertToReversePolishNotation(formula);
+        String expectedResult = "12+";
+        assertEquals(expectedResult, factResult);
+    }
+
+    @Test
+    public void test_convert_2() throws Exception{
+        String formula = "1+2*3";
+        String factResult = ReversePolishNotation.convertToReversePolishNotation(formula);
+        String expectedResult = "123*+";
+        assertEquals(expectedResult, factResult);
+    }
+
+}
