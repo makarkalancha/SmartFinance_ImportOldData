eff2fe4b500321bd6fcd495b6f560bc633f841e7
Makar Kalancha <makarkalancha@gmail.com>
2016-06-22 18:51:08 -0400

    polish notation: need optimization

--- src/main/java/com/makco/smartfinance/utils/ReversePolishNotation.java
+++ src/main/java/com/makco/smartfinance/utils/ReversePolishNotation.java
@@ -2,52 +2,106 @@ package com.makco.smartfinance.utils;
 
 import com.makco.smartfinance.utils.collection.DequeStack;
 import com.makco.smartfinance.utils.collection.Stack;
+import org.apache.commons.lang3.CharSetUtils;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by mcalancea on 21 Jun 2016.
  */
 public class ReversePolishNotation {
-    private Stack<Character> characterStack = new DequeStack<>();
+    private Stack<String> operandStack = new DequeStack<>();
+    private List<String> operandOperatorList = new ArrayList<>();
+    private String allowedCharacters = "/*-+()";
+    private char decimalSeparator;
+    private String arithmeticNotation;
 
-    public static String convertToReversePolishNotation(String arithmeticNotation){
-        StringBuilder reversePolishNotation = new StringBuilder();
-        Stack<Character> characterStack = new DequeStack<>();
-//        arithmeticNotation.chars().forEach(intConsumer -> {
-        char[] chars = arithmeticNotation.toCharArray();
-        for (int i = 0; i < chars.length; i++) {
+    public ReversePolishNotation(String arithmeticNotation, char decimalSeparator){
+        this.arithmeticNotation = arithmeticNotation;
+        this.decimalSeparator = decimalSeparator;
+    }
 
-            System.out.println(">>intConsumer:" + intConsumer);
-            switch ((char) intConsumer) {
-                case '+':
-                case '-':
-                case '*':
-                case '/':
-                case '(':
-                    characterStack.push(Character.valueOf((char) intConsumer));
-                    break;
-//                case '*':
-//                case '/':
-//                    reversePolishNotation.append(intConsumer);
-//                    break;
-                case ')':
-                    reversePolishNotation.append(characterStack.pop().toString());
-                    break;
-                case '1':
-                case '2':
-                case '3':
-                case '4':
-                case '5':
-                case '6':
-                case '7':
-                case '8':
-                case '9':
-                case '0':
-                    reversePolishNotation.append(intConsumer);
-                    break;
+    public char getDecimalSeparator() {
+        return decimalSeparator;
+    }
+
+    public void setDecimalSeparator(char decimalSeparator) {
+        this.decimalSeparator = decimalSeparator;
+    }
+
+    public String getArithmeticNotation() {
+        return arithmeticNotation;
+    }
+
+    public void setArithmeticNotation(String arithmeticNotation) {
+        this.arithmeticNotation = arithmeticNotation;
+    }
+
+    private void convertStringToOperandList(){
+        StringBuilder operand = new StringBuilder();
+        for (int i = 0; i < arithmeticNotation.length(); i++) {
+            char charInString = arithmeticNotation.charAt(i);
+            if (allowedCharacters.indexOf(charInString) > -1) {
+
+                operandOperatorList.add(operand.toString());
+                operand = new StringBuilder();
+
+                operandOperatorList.add(Character.toString(charInString));
+
+            } else {
+                operand.append(charInString);
             }
-//        });
         }
-        System.out.println(">>>characterStack: " + characterStack);
+        operandOperatorList.add(operand.toString());
+    }
+
+    public String convertToReversePolishNotation(){
+        convertStringToOperandList();
+        System.out.println(operandOperatorList);
+
+        StringBuilder reversePolishNotation = new StringBuilder();
+        for (int i = 0; i < operandOperatorList.size(); i++) {
+            String element = operandOperatorList.get(i);
+            if (element.equals("+") ||
+                    element.equals("-") ||
+                    element.equals("*") ||
+                    element.equals("/") ||
+                    element.equals("(")
+                    ) {
+                operandStack.push(element);
+            } else if (element.equals(")")) {
+                if (!operandStack.isEmpty() && operandStack.peek().equals("(")) {
+                    operandStack.pop(); //remove "("
+                }
+                if (!operandStack.isEmpty() && !operandStack.peek().equals("(")) {
+                    reversePolishNotation.append(operandStack.pop());
+                }
+            } else {
+                reversePolishNotation.append(element);
+
+                if (!operandStack.isEmpty() && operandOperatorList.size() > (i + 1)) {
+                    String nextOperand = operandOperatorList.get(i + 1);
+                    if (operandStack.peek().equals("+") || operandStack.peek().equals("-")) {
+                        if (!nextOperand.equals("(") &&
+                                !nextOperand.equals("*") &&
+                                !nextOperand.equals("/")) {
+                            reversePolishNotation.append(operandStack.pop());
+                        }
+                    }else if (!nextOperand.equals("(") && (operandStack.peek().equals("*") || operandStack.peek().equals("/"))) {
+                        reversePolishNotation.append(operandStack.pop());
+                    }
+                }
+            }
+
+            if (i == (operandOperatorList.size() - 1)) {
+                while(!operandStack.isEmpty()){
+                    reversePolishNotation.append(operandStack.pop());
+                }
+            }
+        }
+        System.out.println(">>>reversePolishNotation: " + reversePolishNotation.toString());
         return reversePolishNotation.toString();
     }
 
--- src/test/java/com/makco/smartfinance/utils/ReversePolishNotationTest.java
+++ src/test/java/com/makco/smartfinance/utils/ReversePolishNotationTest.java
@@ -14,7 +14,8 @@ public class ReversePolishNotationTest {
     @Test
     public void test_convert_1() throws Exception{
         String formula = "1+2";
-        String factResult = ReversePolishNotation.convertToReversePolishNotation(formula);
+        ReversePolishNotation rpn = new ReversePolishNotation(formula, BigDecimalUtils.getDecimalSeparator());
+        String factResult = rpn.convertToReversePolishNotation();
         String expectedResult = "12+";
         assertEquals(expectedResult, factResult);
     }
@@ -22,7 +23,8 @@ public class ReversePolishNotationTest {
     @Test
     public void test_convert_2() throws Exception{
         String formula = "1+2-3";
-        String factResult = ReversePolishNotation.convertToReversePolishNotation(formula);
+        ReversePolishNotation rpn = new ReversePolishNotation(formula, BigDecimalUtils.getDecimalSeparator());
+        String factResult = rpn.convertToReversePolishNotation();
         String expectedResult = "12+3-";
         assertEquals(expectedResult, factResult);
     }
@@ -30,7 +32,8 @@ public class ReversePolishNotationTest {
     @Test
     public void test_convert_3() throws Exception{
         String formula = "1*2/3";
-        String factResult = ReversePolishNotation.convertToReversePolishNotation(formula);
+        ReversePolishNotation rpn = new ReversePolishNotation(formula, BigDecimalUtils.getDecimalSeparator());
+        String factResult = rpn.convertToReversePolishNotation();
         String expectedResult = "12*3/";
         assertEquals(expectedResult, factResult);
     }
@@ -38,7 +41,8 @@ public class ReversePolishNotationTest {
     @Test
     public void test_convert_4() throws Exception{
         String formula = "1+2*3";
-        String factResult = ReversePolishNotation.convertToReversePolishNotation(formula);
+        ReversePolishNotation rpn = new ReversePolishNotation(formula, BigDecimalUtils.getDecimalSeparator());
+        String factResult = rpn.convertToReversePolishNotation();
         String expectedResult = "123*+";
         assertEquals(expectedResult, factResult);
     }
@@ -46,7 +50,8 @@ public class ReversePolishNotationTest {
     @Test
     public void test_convert_5() throws Exception{
         String formula = "1*2+3";
-        String factResult = ReversePolishNotation.convertToReversePolishNotation(formula);
+        ReversePolishNotation rpn = new ReversePolishNotation(formula, BigDecimalUtils.getDecimalSeparator());
+        String factResult = rpn.convertToReversePolishNotation();
         String expectedResult = "12*3+";
         assertEquals(expectedResult, factResult);
     }
@@ -54,7 +59,8 @@ public class ReversePolishNotationTest {
     @Test
     public void test_convert_6() throws Exception{
         String formula = "1*(2+3)";
-        String factResult = ReversePolishNotation.convertToReversePolishNotation(formula);
+        ReversePolishNotation rpn = new ReversePolishNotation(formula, BigDecimalUtils.getDecimalSeparator());
+        String factResult = rpn.convertToReversePolishNotation();
         String expectedResult = "123+*";
         assertEquals(expectedResult, factResult);
     }
@@ -62,15 +68,24 @@ public class ReversePolishNotationTest {
     @Test
     public void test_convert_7() throws Exception{
         String formula = "1*2+3*4";
-        String factResult = ReversePolishNotation.convertToReversePolishNotation(formula);
+        ReversePolishNotation rpn = new ReversePolishNotation(formula, BigDecimalUtils.getDecimalSeparator());
+        String factResult = rpn.convertToReversePolishNotation();
         String expectedResult = "12*34*+";
         assertEquals(expectedResult, factResult);
     }
 
+//    @Test
+//    public void test_equalString() throws Exception{
+//        String nullString = null;
+//        boolean equal = nullString.equals("something");
+//        assertEquals(false, equal);
+//    }
+
     @Test
     public void test_convert_8() throws Exception{
         String formula = "(1+2)*(3-4)";
-        String factResult = ReversePolishNotation.convertToReversePolishNotation(formula);
+        ReversePolishNotation rpn = new ReversePolishNotation(formula, BigDecimalUtils.getDecimalSeparator());
+        String factResult = rpn.convertToReversePolishNotation();
         String expectedResult = "12+34-*";
         assertEquals(expectedResult, factResult);
     }
@@ -78,7 +93,8 @@ public class ReversePolishNotationTest {
     @Test
     public void test_convert_9() throws Exception{
         String formula = "((1+2)*3)-4";
-        String factResult = ReversePolishNotation.convertToReversePolishNotation(formula);
+        ReversePolishNotation rpn = new ReversePolishNotation(formula, BigDecimalUtils.getDecimalSeparator());
+        String factResult = rpn.convertToReversePolishNotation();
         String expectedResult = "12+3*4-";
         assertEquals(expectedResult, factResult);
     }
@@ -86,7 +102,8 @@ public class ReversePolishNotationTest {
     @Test
     public void test_convert_10() throws Exception{
         String formula = "1+2*(3-4/(5+6))";
-        String factResult = ReversePolishNotation.convertToReversePolishNotation(formula);
+        ReversePolishNotation rpn = new ReversePolishNotation(formula, BigDecimalUtils.getDecimalSeparator());
+        String factResult = rpn.convertToReversePolishNotation();
         String expectedResult = "123456+/-*+";
         assertEquals(expectedResult, factResult);
     }
