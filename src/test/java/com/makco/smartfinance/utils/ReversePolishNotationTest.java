66bf8643df423267ed6ead8ff79fca736ee6d61a
Makar Kalancha <makarkalancha@gmail.com>
2016-06-22 14:52:49 -0400

    polish notation: tests

--- src/test/java/com/makco/smartfinance/utils/ReversePolishNotationTest.java
+++ src/test/java/com/makco/smartfinance/utils/ReversePolishNotationTest.java
@@ -21,9 +21,73 @@ public class ReversePolishNotationTest {
 
     @Test
     public void test_convert_2() throws Exception{
+        String formula = "1+2-3";
+        String factResult = ReversePolishNotation.convertToReversePolishNotation(formula);
+        String expectedResult = "12+3-";
+        assertEquals(expectedResult, factResult);
+    }
+
+    @Test
+    public void test_convert_3() throws Exception{
+        String formula = "1*2/3";
+        String factResult = ReversePolishNotation.convertToReversePolishNotation(formula);
+        String expectedResult = "12*3/";
+        assertEquals(expectedResult, factResult);
+    }
+
+    @Test
+    public void test_convert_4() throws Exception{
         String formula = "1+2*3";
         String factResult = ReversePolishNotation.convertToReversePolishNotation(formula);
         String expectedResult = "123*+";
+        assertEquals(expectedResult, factResult);
+    }
+
+    @Test
+    public void test_convert_5() throws Exception{
+        String formula = "1*2+3";
+        String factResult = ReversePolishNotation.convertToReversePolishNotation(formula);
+        String expectedResult = "12*3+";
+        assertEquals(expectedResult, factResult);
+    }
+
+    @Test
+    public void test_convert_6() throws Exception{
+        String formula = "1*(2+3)";
+        String factResult = ReversePolishNotation.convertToReversePolishNotation(formula);
+        String expectedResult = "123+*";
+        assertEquals(expectedResult, factResult);
+    }
+
+    @Test
+    public void test_convert_7() throws Exception{
+        String formula = "1*2+3*4";
+        String factResult = ReversePolishNotation.convertToReversePolishNotation(formula);
+        String expectedResult = "12*34*+";
+        assertEquals(expectedResult, factResult);
+    }
+
+    @Test
+    public void test_convert_8() throws Exception{
+        String formula = "(1+2)*(3-4)";
+        String factResult = ReversePolishNotation.convertToReversePolishNotation(formula);
+        String expectedResult = "12+34-*";
+        assertEquals(expectedResult, factResult);
+    }
+
+    @Test
+    public void test_convert_9() throws Exception{
+        String formula = "((1+2)*3)-4";
+        String factResult = ReversePolishNotation.convertToReversePolishNotation(formula);
+        String expectedResult = "12+3*4-";
+        assertEquals(expectedResult, factResult);
+    }
+
+    @Test
+    public void test_convert_10() throws Exception{
+        String formula = "1+2*(3-4/(5+6))";
+        String factResult = ReversePolishNotation.convertToReversePolishNotation(formula);
+        String expectedResult = "123456+/-*+";
         assertEquals(expectedResult, factResult);
     }
 
