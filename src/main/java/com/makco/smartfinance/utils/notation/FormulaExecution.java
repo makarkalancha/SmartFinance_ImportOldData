b679dac859a5e84b64ee3f801484f2e209ae2e33
Makar Kalancha <makarkalancha@gmail.com>
2016-06-25 22:26:55 -0400

    formula to read

--- src/main/java/com/makco/smartfinance/utils/notation/FormulaExecution.java
+++ src/main/java/com/makco/smartfinance/utils/notation/FormulaExecution.java
@@ -6,6 +6,15 @@ import javax.script.ScriptEngineManager;
 /**
  * Created by mcalancea on 23 Jun 2016.
  */
+
+/*
+todo read about formula evaluation
+http://andreas.haufler.info/2013/12/how-to-write-one-of-fastest-expression.html
+http://www.transylvania-jug.org/archives/5777
+https://dzone.com/articles/even-faster-java-expression
+https://udojava.com/2012/12/16/java-expression-parser-evaluator/
+
+ */
 public class FormulaExecution<T> {
     private ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
     private final String arithmeticExpression;
