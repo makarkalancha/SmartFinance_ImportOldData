37d16fb84151705cf0db6ae2293ceead21d80fe6
Makar Kalancha <makarkalancha@gmail.com>
2016-07-27 17:08:40 -0400

    transaction v3

---
+++ src/test/java/com/makco/smartfinance/persistence/dao/TransactionDAOImplTest_v3.java
@@ -0,0 +1,15 @@
+package com.makco.smartfinance.persistence.dao;
+
+import org.junit.FixMethodOrder;
+import org.junit.runners.MethodSorters;
+
+/**
+ * User: Makar Kalancha
+ * Date: 27/07/2016
+ * Time: 16:36
+ */
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class TransactionDAOImplTest_v3 {
+    //TODO p386 and section 20.1 cursor and batch processing: list too large
+
+}
